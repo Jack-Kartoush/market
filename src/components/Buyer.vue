4a10d2ef213fc2cede2378008fb7587b9dbf703e
Jack Kartoush <jackkartosh@gmail.com>
2023-02-24 09:24:06 +0100

    adding the returnItem button

--- src/components/Buyer.vue
+++ src/components/Buyer.vue
@@ -30,7 +30,8 @@
       </div>
       <div class="highest_bid text-white p-3 text-center">
         <span>Highest Bid</span>
-        <p>300$</p>
+
+        <p v-for="bid in limit">{{ bid.userBid }}</p>
       </div>
     </div>
     <div class="navbar d-block">
@@ -41,7 +42,10 @@
             History
           </button>
         </div>
-        <button class="btn btn-outline-danger deleteBtn" @click="deleteAllItem()">
+        <button
+          class="btn btn-outline-danger deleteBtn"
+          @click="deleteAllItem()"
+        >
           Delete All
         </button>
       </div>
@@ -56,7 +60,7 @@
         <button
           v-show="tab === 'bid'"
           class="btn btn-primary"
-          @click="Addbid()"
+          @click="addBid()"
         >
           Add
         </button>
@@ -65,63 +69,76 @@
   </section>
   <section class="bid_section" v-show="tab === 'bid'">
     <div
-      v-for=" (bid,index) in bids"
+      v-for="(bid, index) in bids"
       class="d-flex justify-content-around align-items-center text-center p-3 bg-secondary text-white border-bottom"
     >
-      <!-- <span>{{ bid.bidID }}</span> -->
       <span>{{ bid.userBid }} $</span>
       <span>{{ bid.currentDate }}</span>
-      <button class="btn btn-primary" @click="deleteBidItem(index)"><i class="fa-solid fa-trash-can"></i></button>
-    
+      <button class="btn btn-primary" @click="deleteBidItem(index)">
+        <i class="fa-solid fa-trash-can"></i>
+      </button>
     </div>
   </section>
-  <section v-show="tab === 'History'">
+  <section class="bid_section" v-show="tab === 'History'">
     <h1 class="text-center p-3">History</h1>
     <div
-      v-for=" (history,index) in historys"
+      v-for="(history, index) in historys"
       class="d-flex justify-content-around align-items-center text-center p-3 bg-secondary text-white border-bottom"
     >
-      <!-- <span>{{ history.bidID }}</span> -->
+      <span>{{ history.ID }}</span>
       <span>{{ history.userBid }} $</span>
       <span>{{ history.currentDate }}</span>
-      <button class="btn btn-primary" @click="deleteHistoryItem(index)"><i class="fa-solid fa-trash-can"></i></button>
-      <button class="btn btn-primary" title="return it to the bid records"><i class="fa-solid fa-rotate-left"></i></button>
+      <button class="btn btn-primary" @click="deleteHistoryItem(index)">
+        <i class="fa-solid fa-trash-can"></i>
+      </button>
+      <button
+        class="btn btn-primary"
+        title="return it to the bid records"
+        @click="returnDeletedItems(index)"
+        v-show="!bids.filter((bid) => bid.ID === history.ID).length"
+      >
+        <i class="fa-solid fa-rotate-left"></i>
+      </button>
     </div>
   </section>
 </template>
 <script setup>
-import { ref, onMounted, defineProps, toRefs, defineEmits } from "vue";
+import { ref, onMounted, defineProps, toRefs, defineEmits,computed  } from "vue";
 const props = defineProps(["showUser"]);
 const emit = defineEmits(["seller", "setUserTo"]);
 const { showUser } = toRefs(props);
 const bids = ref([]);
 const historys = ref([]);
 const userBid = ref();
-const bidID = ref(1);
+let ID = 0;
 const tab = ref("bid");
 const currentDate = new Date().toISOString().slice(0, 10);
 onMounted(() => {
   bids.value = JSON.parse(localStorage.getItem("bids") || "[]");
   historys.value = JSON.parse(localStorage.getItem("historys") || "[]");
+  console.log("bids", bids.value);
+  console.log("history", historys.value);
 });
 
-function Addbid() {
+function addBid() {
+
   bids.value.push({
+    ID: ID,
     userBid: userBid.value,
-    bidID: bidID.value,
     currentDate: currentDate,
   });
+  
   localStorage.setItem("bids", JSON.stringify(bids.value));
 
-
   historys.value.push({
+    ID: ID,
     userBid: userBid.value,
-    bidID: bidID.value,
     currentDate: currentDate,
   });
   localStorage.setItem("historys", JSON.stringify(historys.value));
-  bidID.value++;
-  userBid.value = null;
+  
+   ID++;
+   userBid.value = null;
 }
 
 function deleteAllItem() {
@@ -129,13 +146,21 @@ function deleteAllItem() {
   localStorage.removeItem("historys");
   location.reload();
 }
-function deleteBidItem(index){
-  bids.value.splice(index,1)
+function deleteBidItem(index) {
+  bids.value.splice(index, 1);
   localStorage.setItem("bids", JSON.stringify(bids.value));
-
 }
-function deleteHistoryItem(index){
-  historys.value.splice(index,1)
+function deleteHistoryItem(index) {
+  historys.value.splice(index, 1);
   localStorage.setItem("historys", JSON.stringify(historys.value));
 }
+function returnDeletedItems(index) {
+  bids.value.push(historys.value[index]);
+  localStorage.setItem("bids", JSON.stringify(bids.value));
+}
+
+const limit = computed( () =>{
+  const sortBids = bids.value.sort((a, b) => b.userBid - a.userBid)
+          return sortBids.slice(0, 1)
+})
 </script>
